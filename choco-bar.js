9b56da4fefb31c308e4a7dd3067ebb2774216fdd
Carlo Refice <CRefice@gmail.com>
2022-05-20 17:50:06 +0200

    Slightly improve layout

--- choco-bar.js
+++ choco-bar.js
@@ -13,7 +13,10 @@ const logo = document.querySelector('#logo');
 
 const tl = new TimelineMax();
 
+/*
 tl.fromTo(hero, 1.2, { height: "100%" }, { height: "250%", ease: Power2.easeInOut })
     .fromTo(hero, 1.2, { width: "100%" }, { height: "250%", ease: Power2.easeInOut }, "-=1.2")
     .fromTo(hero, 2.0, { x: "0%" }, { x: "-300%", ease: Power2.easeInOut }, "-=0.0")
     .fromTo(slider, 0.7, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut }, "-=0.5")
+
+*/
